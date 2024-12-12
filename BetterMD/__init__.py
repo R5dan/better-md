168c2500e94005d80147afddf8f110dc8ba12c8c
R5dan <r5dan@outlook.com>
2024-12-12 12:55:11 +0000

    Import all elements explicitly

--- BetterMD/__init__.py
+++ BetterMD/__init__.py
@@ -1,4 +1,4 @@
-from .elements import *
+from .elements import A, H1, H2, H3, H4, H5, H6, Head, OL, UL, LI, Text, Div, P, Span, Img, B, I, Br, Blockquote, Hr, Table, Tr, Td, Th, Input, Code
 from .html import CustomHTML
 from .markdown import CustomMarkdown
 from .rst import CustomRst
